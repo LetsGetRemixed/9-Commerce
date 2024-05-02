d94201cbaab1a9984c1ba36c43dd996ec098a01e
Colby Person <73035961+LetsGetRemixed@users.noreply.github.com>
2024-05-02 18:38:18 -0500

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,15 +3,7 @@
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyADS13o5tchNQT3L84tazj2ul89GKz5V04",
-//   authDomain: "ecommerce-4ac55.firebaseapp.com",
-//   projectId: "ecommerce-4ac55",
-//   storageBucket: "ecommerce-4ac55.appspot.com",
-//   messagingSenderId: "229087310072",
-//   appId: "1:229087310072:web:dfcd788f92dc95a57c8b0d"
-// };
+
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
